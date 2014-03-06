a3dc0cb83817a352f225cfbb57e4e1db567ad4b9
Three If By Whiskey <3ifbyw@gmail.com>
2014-03-05 21:43:44 -0500

    Fix alignment

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -16,5 +16,5 @@ $___ = '' << $-_ * $- - $- << $-_ * ($- + @_) << @__ << @__ # Fizz
        : $. % $-  == __ ? @___        # "Buzz" for 5
        : $.) <<                       # Otherwise, n
        ('' << $- * @_)                # and a newline
-  $. < ($- * @_) ** @_ ? ___[] : _   # Check n against 100
+  $. < ($- * @_) ** @_ ? ___[] : _    # Check n against 100
 })[] # Immediately invoke the lambda.
