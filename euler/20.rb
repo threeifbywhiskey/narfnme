98929d12ca47a2428f9af5aba2bf0ccd0d9aadfb
Three If By Whiskey <3ifbyw@gmail.com>
2014-03-11 20:43:54 -0400

    Refactor Euler #20 solution to not use $-

--- euler/20.rb
+++ euler/20.rb
@@ -1,16 +1,16 @@
-_  = $$ / $$ #  1
-__ =  _ +  _ #  2
-$. =  _ + __ #  3
-$- = $. + __ #  5
-$_ = __ * $- # 10
+_  = $$ / $$  #  1
+__ =  _ +  _  #  2
+$. =  _ + __  #  3
+$-_= $. + __  #  5
+$_ = __ * $-_ # 10
 
 # This Proc takes one parameter and returns its digital sum.
 @__ = -> _ {     # n = _
   @_ = _ - _     # sum = 0
-  ($- = -> {     # inner loop
+  ($-_ = -> {     # inner loop
     @_ += _ % $_ # sum += n % 10
     _ /= $_      # n /= 10
-    _ == _ - _ ? @_ : $-[] # Return sum if n is 0 or go again.
+    _ == _ - _ ? @_ : $-_[] # Return sum if n is 0 or go again.
   })[] # Call inner loop immediately.
 }
 
